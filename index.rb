8092272c9c477edfb4fd97cab6298e5ec925b2cc
l2083745 <37018818+l2083745@users.noreply.github.com>
2018-03-17 22:31:13 +0900

    Create index.rb

---
+++ index.rb
@@ -0,0 +1,64 @@
+<html>
+<img src="">
+<head>
+ <meta charset="UTF-8">
+</head>
+
+<img src="https://2.bp.blogspot.com/-TrF2hV0zpk8/WUJWGn7SULI/AAAAAAABE1c/kgFmebjLUcYJzSMwdZVojkKzWKPpox64ACLcBGAs/s1600/top_banner.jpg" height="400"><br><br>
+
+<body bgcolor="#F6CECE" text="#000000" link="#0000ff" vlink="#800080" alink="#ff0000">
+
+
+
+</body>
+
+ <h1>投稿ページ</h1>
+ <form action="/comment_send" method="POST">
+   <div>
+   <img src="https://yt3.ggpht.com/-yPtC64Hkdbw/AAAAAAAAAAI/AAAAAAAAAAA/jnw_WDLq4Kg/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg" width="60" height="60" 
+   style="border:solid 5px #F78181">
+   </div>
+   <div>
+    <input type="text" name="name" value="" placeholder="名前を入力してください">
+   </div>
+   <div>
+   <input type="button" value="チャンネル" onClick="window.open('http://google.com')" style="border:solid 5px #000000">
+   </div>
+  
+   <div>
+     <input type="text" name="title" value="" placeholder="タイトルを入力してください">
+   </div>
+ 
+   <div>
+   <input type="text" name="equipment" value="" placeholder="必要機材">
+   </div>
+
+ <form action="cgi-bin/formmail.cgi" method="post">
+
+ <div>
+<p>開発期間：
+<select name="h">
+<option value="Aries">おひつじ座</option>
+<option value="Taurus">おうし座</option>
+<option value="Gemini">ふたご座</option>
+<option value="Cancer">かに座</option>
+<option value="Leo" selected>しし座</option>
+<option value="Virgo">おとめ座</option>
+<option value="Libra">てんびん座</option>
+<option value="Scorpio" disabled>さそり座</option>
+<option value="Saggitarius">いて座</option>
+<option value="Capricorn">やぎ座</option>
+<option value="Aquarius">みずがめ座</option>
+<option value="Pisces">うお座</option>
+</select></p>
+</div>
+ 
+   <div>
+     <textarea name="message" rows="20" value="" placeholder="開発工程を入力してください" ></textarea>
+   </div>
+   <div>
+     <input type="submit" value="投稿" style="border:solid 5px #F78181">
+   </div>
+ </form>
+</body>
+</html>
